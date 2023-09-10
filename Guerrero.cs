7e9c23a50121622d57e484cde818e8e8488ffd3d
Felix Ochoa <fdavidochoa@unicesar.edu.co>
2023-09-10 18:41:12 -0500

    Agregar archivos de proyecto.

---
+++ Guerrero.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Videojuego
+{
+    internal class Guerrero : Personaje
+    {
+        public int Espada;
+
+        public override int Atacar(Personaje personaje)
+        {
+            int daño = Espada + Fuerza;
+            
+            if(personaje.Defensa > 0 && personaje.Armadura > 0)
+            {
+                int defensaTotal = personaje.Defensa + personaje.Armadura;
+
+                if(defensaTotal < daño)
+                {
+                    daño = daño - defensaTotal;
+
+
+                    personaje.Vida = personaje.Vida - daño;
+                }
+            }
+            else
+            {
+                personaje.Vida = personaje.Vida - daño;
+            }
+            
+            return daño;
+        }
+
+        public override void MostrarSalud()
+        {
+            Console.WriteLine($"La vida del personaje {Nombre} es: {Vida}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Videojuego
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Personaje guerrero1 = new Guerrero()
+            {
+                Vida = 500,
+                Fuerza = 28,
+                Espada = 83,
+                Armadura = 30,
+                Defensa = 23,
+                Inteligencia = 40,
+                Nombre = "Felix"
+            };
+            Personaje guerrero2 = new Guerrero()
+            {
+                Vida = 600,
+                Fuerza = 44,
+                Espada = 69,
+                Armadura = 20,
+                Defensa = 20,
+                Inteligencia = 34,
+                Nombre = "Juan"
+            };
+
+            bool controlador = true;
+
+            while (controlador)
+            {
+                guerrero1.Turno(guerrero2);
+
+                guerrero1.MostrarSalud();
+                guerrero2.MostrarSalud();
+
+                Console.WriteLine();
+                Console.ReadKey();
+            }
+
+            
+        }
+    }
+}
